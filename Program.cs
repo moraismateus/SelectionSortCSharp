68cf7f8e63d9d5962c3a1185f3c8e9d3a50734d5
Mateus Morais <mateus87morais@gmail.com>
2022-08-30 19:59:13 -0700

    Updating committed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var unsortedValues = new int[] { 29, 929, 44, 423, 3214, 64, 12, 662, 2, 1, 7, 79, 22, 41, 3, 112, 1 };
+Console.WriteLine("Unsorted Items:");
+Console.WriteLine(string.Join(", ", unsortedValues));
+var sortedList = SelectionSortCode.GetSelectionSort(unsortedValues);
+Console.WriteLine(string.Join(", ", sortedList));
+
